277bd82d2e8859b7422b357dfdde06f71faede9b
Luciano-Tablet <luciano.jung@mnd.thm.de>
2018-12-26 02:05:23 +0100

    make a few bugfix

--- src/de/jung/luciano/photviewer/Diashow.java
+++ src/de/jung/luciano/photviewer/Diashow.java
@@ -1,16 +1,70 @@
 package de.jung.luciano.photviewer;
 
+import javafx.geometry.Dimension2D;
 import javafx.scene.Scene;
+import javafx.scene.control.Menu;
+import javafx.scene.control.MenuBar;
+import javafx.scene.control.MenuItem;
+import javafx.scene.image.ImageView;
+import javafx.scene.layout.BorderPane;
+import javafx.scene.paint.Color;
+import javafx.scene.paint.Paint;
 import javafx.stage.Stage;
+
 
 public class Diashow {
     private Scene scene;
+    private final Dimension2D dimension = new Dimension2D(1000, 600);
+    private BorderPane borderPane;  //root
+    //MenuBar (TOP)
+    private MenuBar menuBar;        //menuBar
+    private Menu menuAction;
+    private MenuItem menuItemPauseDiashow;
+    private MenuItem menuItemStopDiashow;
+    //for CenterImage (CENTER)
+    private ImageView imageView = new ImageView();
 
-    //Nodes...
+    public Diashow(){
+        //BorderPane Layout
+        borderPane = new BorderPane();
+
+        //--create MenuBar--
+        menuItemPauseDiashow = new MenuItem("Pause");
+        menuItemStopDiashow = new MenuItem("Stop");
+        menuAction = new Menu("Aktion");
+        menuAction.getItems().addAll(menuItemPauseDiashow, menuItemStopDiashow);
+        menuBar = new MenuBar();
+        menuBar.getMenus().addAll(menuAction);
+        borderPane.setTop(menuBar);
+
+        //--Center for chosen Picture--
+        imageView.setPreserveRatio(true);
+        borderPane.setCenter(imageView);
+
+        //set Scene
+        scene = new Scene(borderPane,  dimension.getWidth(), dimension.getHeight());
+
+    }
 
     public void show(Stage primaryStage) {
         primaryStage.setTitle("Diashow");
         primaryStage.setScene(scene);
         primaryStage.show();
+    }
+
+    public MenuItem getMenuItemPauseDiashow() {
+        return menuItemPauseDiashow;
+    }
+
+    public MenuItem getMenuItemStopDiashow() {
+        return menuItemStopDiashow;
+    }
+
+    public ImageView getImageView() {
+        return imageView;
+    }
+
+    public Scene getScene() {
+        return scene;
     }
 }
--- src/de/jung/luciano/photviewer/DiashowController.java
+++ src/de/jung/luciano/photviewer/DiashowController.java
@@ -1,21 +1,129 @@
 package de.jung.luciano.photviewer;
 
+import javafx.beans.property.*;
+import javafx.event.ActionEvent;
+import javafx.scene.image.Image;
+
+import javax.swing.text.html.ImageView;
+
+import java.awt.event.InputEvent;
+import java.awt.event.KeyEvent;
+import java.security.Key;
+
+import static javafx.application.Platform.runLater;
+
 public class DiashowController {
     //Model
     Model model;
 
     //View
     Diashow diashowView;
+
+    //Image Task
+    ImageTask imageTask;
+    private Thread imageThread  = null;
+
     
     public DiashowController(Model model) {
         this.model = model;
         this.diashowView = new Diashow();
+        imageTask = new ImageTask();
+
+        //Binding
+        diashowView.getImageView().fitWidthProperty().bind( model.getPrimaryStage().widthProperty());
+        diashowView.getImageView().fitHeightProperty().bind(model.getPrimaryStage().heightProperty());
 
         //Listener
-
+        diashowView.getMenuItemPauseDiashow().setOnAction(event -> handlePauseDiashow(event));
+        diashowView.getMenuItemStopDiashow().setOnAction(event -> handleStopDiashow(event));
+        diashowView.getScene().setOnKeyPressed(event -> {           //Handle Key Pressed ESCAPE while Diashow is shown
+            if (event.getCode().toString().equals("ESCAPE"))
+                handleStopDiashow(new ActionEvent());               //-> back to photoviewer
+        });
     }
 
+    //+++++++++++++++++++++++++++++
+    //Listener
+    //+++++++++++++++++++++++++++++
+
+    //stops the Diashow (names are contradictory)
+    private void handlePauseDiashow(ActionEvent event) {
+        if (imageThread.getState().equals(Thread.State.TERMINATED))
+            handleStartDiashow(event);
+        else
+            handleInterruptDiashow(event);
+    }
+
+    //stops diahow via handlePause and return to photoviewer scene
+    private void handleStopDiashow(ActionEvent event) {
+        handleInterruptDiashow(event);
+        PhotoViewController photoViewController = new PhotoViewController(model);       //give model to diashowController
+        photoViewController.show();                                                     //show new Scene (Diashow)
+    }
+
+    public void handleStartDiashow(ActionEvent event){
+        //creates and starts a new Thread
+        imageThread = new Thread(imageTask);
+        imageThread.start();
+        diashowView.getMenuItemPauseDiashow().setText("Pause");
+    }
+
+    private void handleInterruptDiashow(ActionEvent event){
+        imageThread.interrupt();
+        try {
+            imageThread.join();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        diashowView.getMenuItemPauseDiashow().setText("Start");
+    }
+
+    //+++++++++++++++++++++++++++++
+    //other methods
+    //+++++++++++++++++++++++++++++
+
     public void show() {
+        if(model.getImages().size() == 0) return;   //return if there are no Images
+        /*
+        * else Start Diashow Thread
+        * set Image (get number of Image in the List from model.indexOfCenterImage
+        * call show method of view with primary Stage
+        * set FullScreen
+        */
+        handleStartDiashow(new ActionEvent());
+        diashowView.getImageView().setImage(model.getImages().get(model.getIndexOfCenterImage().getValue().intValue()));
         diashowView.show(model.getPrimaryStage());
+        model.getPrimaryStage().setFullScreen(true);
+    }
+
+    //+++++++++++++++++++++++++++++++++++++++++++++++
+    //inner class
+    //+++++++++++++++++++++++++++++++++++++++++++++++
+
+    private class ImageTask implements Runnable{
+
+        @Override
+        public void run() {
+            try{
+                while (true){ //Never Stop automaticly!
+                    Thread.sleep(model.getDiashowDuration());   //sleep duration from model
+                    //set Center image IntProperty to next Image (+1) and update Image
+                    model.getIndexOfCenterImage().set(model.getIndexOfCenterImage().intValue()+1);
+                    updateImage(model.getIndexOfCenterImage().intValue());
+                }
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+
+        //update Image method with runLater(Runnable)
+        private void updateImage(int index)
+        {
+            runLater(new Runnable() {
+                @Override public void run() {
+                    diashowView.getImageView().setImage(model.getImages().get(index));
+                }
+            });
+        }
     }
 }
